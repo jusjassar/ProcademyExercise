38ba7d595d0c55b2ebc1d55222a8e7ab5c24a709
Jus Jassar <jusjassar@gmail.com>
2023-10-06 12:47:52 -0400

    video 89

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProcademyExercise';
+export class AppComponent implements OnInit {
+  title = 'ReactiveForms';
+  reactiveForm!:FormGroup;
+ngOnInit(){
+  this.reactiveForm = new FormGroup({
+    firstname: new FormControl('Jhon'),
+    lastname: new FormControl(null),
+    email: new FormControl(null),
+    gender:new FormControl('male'),
+    hobbies:new FormControl('sports'),
+    country:new FormControl()
+
+  })
+}
+onSubmit(){
+  console.log(this.reactiveForm)
+
+}
 }
